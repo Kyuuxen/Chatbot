101ea96311db7da63d8b9e93a86e17293e8575cb
Kyuuxen <121227859+Kyuuxen@users.noreply.github.com>
2024-11-25 17:41:56 +0800

    Update sms.js

--- modules/scripts/commands/sms.js
+++ modules/scripts/commands/sms.js
@@ -3,14 +3,14 @@ const axios = require('axios');
 module.exports = {
   name: 'freesms',
   description: 'freesms <phoneNumber> <message>.',
-  author: 'Dale/<your name>',
   usePrefix: false,
+  author: 'Dale/<your name>',
   async execute(senderId, args, pageAccessToken, sendMessage) {
     const phoneNumber = args[0];
     const message = args.slice(1).join(' ');
 
     if (!phoneNumber || !message) {
-      return sendMessage(senderId, { text: '❌ 𝗨𝘀𝗮𝗴𝗲: 𝗳𝗿𝗲𝗲𝘀𝗺𝘀 𝗽𝗵𝗼𝗻𝗲 𝗻𝘂𝗺𝗯𝗲𝗿 𝗺𝗲𝘀𝘀𝗮𝗴𝗲' }, pageAccessToken);
+      return sendMessage(senderId, { text: '❌ 𝗨𝘀𝗮𝗴𝗲: 𝗳𝗿𝗲𝗲𝘀𝗺𝘀 𝗽𝗵𝗼ន 𝗻𝘂𝗺𝗯𝗲𝗿 𝗺𝗲𝘀𝘀𝗮𝗴𝗲' }, pageAccessToken);
     }
 
     sendMessage(senderId, { text: '⏳ 𝗣𝗿𝗼𝗰𝗲𝘀𝘀𝗶𝗻𝗴 𝘆𝗼𝘂𝗿 𝗿𝗲𝗾𝘂𝗲𝘀𝘁 𝘁𝗼 𝘀𝗲𝗻𝗱 𝘀𝗺𝘀, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...' }, pageAccessToken);
@@ -29,7 +29,7 @@ module.exports = {
 
       if (status) {
         sendMessage(senderId, { 
-          text: `𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗵𝗮𝘀 𝗯𝗲𝗲𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝘀𝗲𝗻𝘁 ✅ \n\n📬 𝗠𝗲𝘀𝘀𝗮𝗴𝗲: ${messageResponse}\n\n🌎 𝗡𝗲𝘁𝘄𝗼𝗿𝗸: ${sim_network}\n📈 𝗣𝗮𝗿𝘁𝘀: ${message_parts}\n📊 𝗠𝗲𝘀𝘀𝗮𝗴𝗲𝘀 𝗥𝗲𝗺𝗮𝗶𝗻𝗶𝗻𝗴: ${message_remaining.toFixed(2)}\n\n⏰ 𝗔𝘀𝗶𝗮/𝗠𝗮𝗻𝗶𝗹𝗮: ${responseTime}`
+          text: `𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗵𝗮𝘀 𝗯𝗲𝗲𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝘀𝗲𝗻𝘁 ✅ \n\n 𝗠𝗲𝘀𝘀𝗮𝗴𝗲: ${messageResponse}\n\n 𝗡𝗲𝘁𝘄𝗼𝗿𝗸: ${sim_network}\n 𝗣𝗮𝗿𝘁𝘀: ${message_parts}\n 𝗠𝗲𝘀𝘀𝗮𝗴𝗲𝘀 𝗥𝗲𝗺𝗮𝗶𝗻𝗶𝗻𝗴: ${message_remaining.toFixed(2)}\n\n⏰ 𝗔𝘀𝗶𝗮/𝗠𝗮𝗻𝗶𝗹𝗮: ${responseTime}`
         }, pageAccessToken);
       } else {
         sendMessage(senderId, { text: '❌ 𝗙𝗮𝗶𝗹𝗲𝗱 𝘁𝗼 𝘀𝗲𝗻𝗱 𝘁𝗵𝗲 𝗺𝗲𝘀𝘀𝗮𝗴𝗲.' }, pageAccessToken);
@@ -37,7 +37,7 @@ module.exports = {
 
     } catch (error) {
       console.error('Error:', error);
-      sendMessage(senderId, { text: '❌ 𝗙𝗮𝗶𝗹𝗲𝗱 𝘁𝗼 𝘀𝗲𝗻𝗱 𝘁𝗵𝗲 𝗺𝗲𝘀𝘀𝗮𝗴𝗲.' }, pageAccessToken);
+      sendMessage(senderId, { text: '❌ 𝗙𝗮𝗶𝗹𝗲드 𝘁𝗼 𝘀𝗲𝗻𝗱 𝘁𝗵𝗲 𝗺𝗲𝘀𝘀𝗮𝗴𝗲.' }, pageAccessToken);
     }
   }
 };
